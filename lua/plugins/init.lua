64ec99407dba45953329da584392cefb90c19ba3
Rahmatullo Qosimov <80538114+byStrange@users.noreply.github.com>
2024-12-15 17:44:34 +0500

    feat: new plugin restaceannvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,11 @@ return {
     lazy = false
   },
   {
+     'mrcjkb/rustaceanvim',
+     version = '^5', -- Recommended
+    lazy = false
+  },
+  {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
