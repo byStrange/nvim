145fc12ef88706add00a3b0cb50738bbc0a3e0af
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-12-18 12:33:48 +0500

    feat: new keybindings

--- init.lua
+++ init.lua
@@ -103,6 +103,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'jk to go backto normal mode' })
+vim.keymap.set('n', ';', ':', { desc = '; to command mode in normal mode' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
