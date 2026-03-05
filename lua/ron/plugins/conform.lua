0da01bb214b2ea2c89862ad747c986f127290384
Rahmatullo <qosimovrahmatullo006@gmail.com>
2026-03-05 16:30:26 +0500

    change conform bind and some other language formatters added

--- lua/ron/plugins/conform.lua
+++ lua/ron/plugins/conform.lua
@@ -4,7 +4,7 @@ return { -- Autoformat
   cmd = { 'ConformInfo' },
   keys = {
     {
-      '<leader>f',
+      '<leader>fl',
       function()
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
@@ -23,11 +23,21 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       htmldjango = { 'djlint' },
+
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = function(bufnr)
+        if require("conform").get_formatter_info("ruff_format", bufnr).available then
+          return { "ruff_format" }
+        else
+          return { "isort", "black" }
+        end
+      end,
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettierd", stop_after_first = true },
+      html = { "prettierd" },
+
+      ["_"] = { "trim_whitespace" },
     },
   },
 }
